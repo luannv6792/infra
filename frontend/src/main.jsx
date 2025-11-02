c953f5aa05c78afad66ab5a65c2928cf05f89ac1
Nguyen Van Luan <44190998+luannv6792@users.noreply.github.com>
2025-11-02 19:29:16 +0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
+import './transitions.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(<App />)
