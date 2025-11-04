1e998c9d402f520413ce42e35db97c8d344a7014
Nguyen Van Luan <44190998+luannv6792@users.noreply.github.com>
2025-11-04 21:58:14 +0700

    Create App.vue

---
+++ web/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="min-h-screen flex items-center justify-center bg-cover bg-center relative"
+    :style="{ backgroundImage: 'url(/src/assets/bg-tech.jpg)' }"
+  >
+    <div class="absolute inset-0 bg-black bg-opacity-60"></div>
+    <LoginForm />
+  </div>
+</template>
+
+<script setup>
+import LoginForm from './components/LoginForm.vue'
+</script>
